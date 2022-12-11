1df9accdbcc2b883232b07f6f95cb8855a1c764d
OlegSadJktu <plyuha2004@mail.ru>
2022-12-11 13:09:35 +0100

    first commit

---
+++ MarvelAppIOS/MarvelAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppIOSApp.swift
+//  MarvelAppIOS
+//
+//  Created by user225687 on 12/11/22.
+//
+
+import SwiftUI
+@main
+struct MarvelAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
